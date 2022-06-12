e6800449bb96fe6d116a4b7675ff5b228dd56db6
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-12 14:53:53 +0200

    store update
    dodane kartice za izgled jos dodat podatke u kartice i da te kartice se prikazuju jedna po jedna

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,18 +1,67 @@
 <template>
-    <div class="row">
-        <div class="col-8">
-            <cards />
-            <cards />
-        </div>
+  <div class="app">
+    <cards/>
+</div>
+<h1>  {{title}} </h1>
+
 </template>
 <script>
-import cards from '@/components/cards.vue'
+import store from "@/store"
+import {getDocs, collection , db} from "@/firebase"
+import cards from '@/components/cards.vue';
 
-export default({
-    name: 'Store',
-    components: {
-        cards,
-    }
-})
+
+
+export default {
+  components: { cards },
+    
+        data() {
+            return {
+            store,
+            Createdat: "",
+            adress: "", 
+            country: "",  
+            desc: "", 
+            region: "", 
+            renttime: "", 
+            title: "", 
+            value: "", 
+            zip: "", 
+		};
+        },
+
+    
+        
+
+        mounted(){
+            
+            this.dohvatitema();
+
+        },
+
+
+        methods: {
+                   async dohvatitema(){
+                            const querySnapshot = await getDocs(collection(db, "products"));
+                            querySnapshot.forEach((doc) => {
+                            if (doc.id === `${doc.data().renter}`) {
+                            this.Createdat = `${doc.data().Createdat}`;
+                            this.adress = `${doc.data().adress}`;
+                            this.country = `${doc.data().country}`;
+                            this.desc = `${doc.data().desc}`;
+                            this.region = `${doc.data().region}`;
+                            this.renttime = `${doc.data().renttime}`;
+                            this.title = `${doc.data().title}`;
+                            this.value = `${doc.data().value}`;
+                            this.zip = `${doc.data().zip}`;
+                                }
+                            });
+                          
+                            
+                    }
+
+      }
+};
+
 </script>
 
