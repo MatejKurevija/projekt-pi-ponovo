1d832027355e764fd0d28952cd319f249beb08bf
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-12 14:54:49 +0200

    update login
    Login zavrsen u slucaju da bude potrebno jos dodavat moguce je

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
+    <h1>Login</h1>
     <div class = "container">
       <div class = "row">
           <div class = "col-sm"></div>
@@ -8,21 +8,50 @@
             <form>
               <div class = "form-group">
                 <label for = "exampleInputEmail">Email adress</label>
-                <input type = "email" class = "form-control" id = "exampleInputEmail" aria-describedby="emailHelp" placeholder = "Enter email" />
+                <input type = "email" class = "form-control" v-model="email" id = "exampleInputEmail" aria-describedby="emailHelp" placeholder = "Enter email" />
                 
               </div>
               <div class = "form group">
                 <label for = "exampleInputPassword">Password</label>
-                <input type = "password" class = "form-control" id = "exampleInputPassword"
+                <input type = "password" class = "form-control" v-model="password" id = "exampleInputPassword"
                 placeholder = "Password"/>
               </div>
-              <button type= "submit" class = " btn btn-primary">Submit</button>
+              <button type= "button" @click="login" class = " btn btn-primary">Log in</button>
             </form>
             <div class = "text"><p>Dont have an account?<a href = "/signup"> Sign up TODAY!</a></p>
             </div>
+            <div class = "text"><p>Forgot your password?<a href = "/passwordresset">Reset your password</a></p>
+            </div>
           </div>
+          
           <div class =" col-sm"></div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { auth, signInWithEmailAndPassword } from "@/firebase";
+
+export default {
+	name: "login",
+	data() {
+		return {
+			email: "",
+			password: "",
+		};
+	},
+	methods: {
+		async login() {
+			try {
+				await signInWithEmailAndPassword(auth, this.email, this.password)
+       
+			} catch (error) {
+				console.log(error.message);
+				console.log(error.code);
+			}
+      this.$router.replace({name: "home"});
+		},
+	},
+};
+</script>
