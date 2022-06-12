0e2ca6d38d1fa38018523c5f1853507d32f38c1a
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-12 14:59:58 +0200

    update za cards

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,3 +1,36 @@
 <template>
-    <p>hello</p>
+    <h1>Welcome to the store!</h1>
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2015%2F04%2F05%2Ffeatured.jpg&q=60" class="card-img-top" alt="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2015%2F04%2F05%2Ffeatured.jpg&q=60">
+      <div class="card-body">
+        <h5 class="card-title">{{title}}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
+
+<script>
+import store from "@/store"
+
+
+export default {
+  	props: [],
+    name: "cards",
+    data () {
+      return{
+        store,
+        
+        
+      }
+    },
+
+
+
+}
+</script>
