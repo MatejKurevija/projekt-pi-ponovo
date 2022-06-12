15ad24c4161105a86985b43cad20beac451a83fb
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-12 14:57:53 +0200

    update za renout
    dodana mogucnost dodavanja proizvoda te jos ostale samo napravit dodavanje slika za odredeni proizvod

--- src/views/rentout.vue
+++ src/views/rentout.vue
@@ -1,48 +1,119 @@
 <template>
-    <form align="center">
-  <div class="form-row">
-    <div class="form-group">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    <div class="container" align="center">
+      <div  class="row">
+        <div class="col">
+ 
+ <h1> Rent out your item </h1>
+  
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Title</span>
+  <input type="text" class="form-control" v-model="title" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Description</span>
+  <textarea class="form-control" v-model="desc" type="text"></textarea>
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Country</span>
+  <input type="text" class="form-control" v-model="country" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">City</span>
+  <input type="text" class="form-control" v-model="city" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Region</span>
+  <input type="text" class="form-control" v-model="region" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Zip</span>
+  <input type="number" class="form-control" v-model="zip" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon3">Adress</span>
+  <input type="text" class="form-control" v-model="adress" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+
+<div class="input-group mb-3">
+  <span class="input-group-text">$</span>
+  <input placeholder="Value of your product" v-model="value" type="number" class="form-control" aria-label="Amount (to the nearest dollar)">
+  
+</div>
+
+
+<div class="input-group mb-3">
+  <input type="file" class="form-control" id="image">
+  <label class="input-group-text"  accept="image/*" for="image">Product image</label>
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text">Available rent time</span>
+  <input placeholder="Type hours days months or years!" type="text" v-model="renttime" class="form-control" aria-label="renttime">
+ 
+</div>
+
+<input class="btn btn-primary" @click="rentout()"  type="Rent out" value="Rent out">
+
+
+ </div>
     </div>
-    <div class="form-group ">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
     </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group ">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group ">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group ">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
 </template>
+
+<script>
+import store from "@/store";
+import {
+   collection,
+   addDoc,
+   db,
+   
+} from '@/firebase';
+
+export default {
+  name: "rentout",
+	data() {
+		return {
+      store,
+			title: "",
+			desc: "",
+			country: "",
+			city: "",
+			region: "",
+			zip: "",
+			adress: "",
+      renttime: "",
+      value: "",
+      createdAt: Date.now(),
+      renter: "",
+		};
+	},
+ 
+  methods: {
+    async rentout(){
+        const rentout = {
+            title: this.title,
+            desc: this.desc,
+            country: this.country,
+            region: this.region,
+            zip: this.zip,
+            adress: this.adress,
+            value: this.value,
+            renttime: this.renttime,
+            renter: this.store.courentuser,
+            Createdat: this.createdAt,
+
+        };
+        await addDoc(collection(db, "products"), rentout);
+     },
+
+ 
+  }
+}
+</script>
