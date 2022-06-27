12c9c0d5f96ccbfa53f89511bce919150b84c57f
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-27 19:51:26 +0200

    Update itemsrented.vue

--- src/views/itemsrented.vue
+++ src/views/itemsrented.vue
@@ -1,63 +1,103 @@
-<template>
-    <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<template v-if="loaded">
+<div class="razmak">
+<h1> Here you can see the items you bought!!</h1>
+</div>
 
-<table class="table">
-  <thead class="thead-light">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+  <div class="app" >
+    <itemsrented 
+    
+    v-for="podatak in podatci "
+    :key="podatak.title"
+    :title="podatak.title"
+    :createdat="podatak.createdat"
+    :adress="podatak.adress"
+    :country="podatak.country"
+    :desc="podatak.desc"
+    :region="podatak.region"
+    :renttime="podatak.renttime"
+    :value="podatak.value"
+    :renter="podatak.renter"
+    >
+    </itemsrented>
+</div>
+
+
 </template>
+<script>
+import store from "@/store"
+import {getDocs, collection , db} from "@/firebase"
+import itemsrented from '@/components/itemsrented'
+
+
+
+export default {
+components: { itemsrented },
+name: itemsrented,
+        data() {
+            return {
+            loaded: false,
+            store,
+            podatci: [],
+
+            
+		};
+        },
+
+    async mounted() {
+            await this.dohvatproizvoda();
+            this.loaded = true;
+            
+    },
+    
+
+ methods: {
+        async dohvatproizvoda(){
+                            const querySnapshot = await getDocs(collection(db, "rented"));
+
+                            let      trenutnipodatci = []
+
+                            querySnapshot.forEach((doc) => {   
+                                if(store.courentuser  == `${doc.data().buyer}`){
+                            trenutnipodatci = {"createdat": `${doc.data().Createdat}`, 
+                                                "title": `${doc.data().title}`,
+                                                "adress": `${doc.data().adress}`,
+                                                "country": `${doc.data().country}`,
+                                                "desc": `${doc.data().desc}`,
+                                                "region": `${doc.data().region}`,
+                                                "renttime": `${doc.data().renttime}`,
+                                                "value": `${doc.data().value}`,
+                                                "buyer": `${doc.data().buyer}`,
+                                                "renter": `${doc.data().renter}`,
+                                                };
+                                
+                                this.podatci.push(trenutnipodatci);
+                                
+                                trenutnipodatci = []; 
+                                
+                                }
+                                },
+                            
+                            ); 
+                           
+                            
+                    }
+                }
+
+};
+
+
+</script>
+
+<style scoped>
+.razmak {
+    
+    
+    text-align: center;
+    font-size: 15px;
+    color: black;
+    border-radius: 25px;
+    margin-top: 25px;
+    margin-bottom: 35px;
+    
+}
+</style>
