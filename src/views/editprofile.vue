e4381ecd61a93c7284668b578e90623ababaf5b9
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-27 19:27:48 +0200

    Update editprofile.vue

--- src/views/editprofile.vue
+++ src/views/editprofile.vue
@@ -1,49 +1,72 @@
 <template>
-    <div class="container space-between">
-        <button class="px-5 me-4 btn btn-sm btn-primary" @click="resetPassword()">Reset password</button>
-        <button align="left" class="px-5 btn btn-sm btn-primary">Delete my profile</button>
-
-    </div>
+<div class="container">
+        
+</div>
   <div class="container mt-3">
     <div class="card p-3 text-center">
         <div class="d-flex flex-row justify-content-center mb-3">
-            <div class="image"> <img height="100" width="100" contain src="https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0" class="rounded-circle"> <span><i class='bx bxs-camera-plus'></i></span> </div>
+            <div class="image" > <img height="100" width="100" contain  :src="imageURL" alt="no profile picture" class="rounded-circle">  </div>
             <div class="d-flex flex-column ms-3 user-details">
-                <h4 class="mb-0">Welcome {{firstName}} {{lastName}}</h4>
-                <div class="ratings"> <span>4.5</span> <i class='bx bx-star ms-1'></i> </div>
+                <h4 class="mb-0">Welcome</h4>
+                <div class="ratings"> <span>Rating: {{avgR}}</span> <i class='bx bx-star ms-1'></i> </div>
             </div>
         </div>
         <h4>Edit Profile</h4>
         <div class="row">
             <div class="col-md-6">
-                <div class="inputs"> <label>Name</label> <input class="form-control" type="text" placeholder="Name"> </div>
+                <div class="inputs"> <label>Name</label> <input v-model="firstName" class="form-control" type="text" placeholder="Name"> </div>
             </div>
             <div class="col-md-6">
-                <div class="inputs"> <label>Email</label> <input class="form-control" type="text" placeholder="Email"> </div>
+                <div class="inputs"> <label>Last name</label> <input  class="form-control" v-model="lastName" type="text" placeholder="Last name"> </div>
             </div>
             <div class="col-md-6">
-                <div class="inputs"> <label>City</label> <input class="form-control" type="text" placeholder="City"> </div>
+                <div class="inputs"> <label>City</label> <input  class="form-control" v-model="city" type="text" placeholder="City"> </div>
             </div>
             <div class="col-md-6">
-                <div class="inputs"> <label>Country</label> <input class="form-control" type="text" placeholder="Country"> </div>
+                <div class="inputs"> <label>State</label> <input  class="form-control" v-model="state" type="text" placeholder="State"> </div>
+            </div>
+            <div class="col-md-6">
+                <div class="inputs"> <label>Adress</label> <input  class="form-control" v-model="adress" type="text" placeholder="Adress"> </div>
+            </div>
+            <div class="col-md-6">
+                <div class="inputs"> <label>Country</label> <input  class="form-control" v-model="country" type="text" placeholder="Country"> </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <div class="about-inputs"> <label>About</label> <textarea class="form-control" type="text" placeholder="{{about}}"> </textarea> </div>
+                <div class="about-inputs"> <label>About</label> <textarea class="form-control" v-model="about" type="text" placeholder="About you"> </textarea> </div>
+            </div>
+            <div class="slika" >
+                <h5> Change your profile picture!</h5>
+            <file-input v-model="imageRef"  is-image ></file-input>
             </div>
         </div>
-        <div class="mt-3 gap-2 d-flex justify-content-end"> <button class="px-3 btn btn-sm btn-outline-primary"><a href="/">Cancel</a></button> <button @click="save()" class="px-3 btn btn-sm btn-primary">Save</button> </div>
+        <div class="mt-3 gap-2 d-flex justify-content-end"> <button class="px-3 btn btn-sm btn-outline-primary"><a href="/">Cancel</a></button> <button @click="save()" class="px-3 btn btn-sm btn-primary">Save</button> 
+        <button class="px-5 me-4 btn btn-sm btn-primary" @click="resetPassword()">Reset password</button>
+        </div>
     </div>
 </div>
 </template>
 
 
 <script>
-import { collection, getDocs, db,  auth, sendPasswordResetEmail } from "@/firebase";
+import { collection,
+         getDocs,
+          db,
+           updateDoc,
+            doc,
+                auth,
+                 sendPasswordResetEmail
+                  } from "@/firebase";
+
+import { uploadBytes, getStorage, getDownloadURL, ref } from '@firebase/storage';
+
 import store from '@/store'
+import FileInput from 'vue3-simple-file-input';
 
 export default {
+    
+    components: { FileInput },
     	name: "EditProfile",
 	data() {
 		return {
@@ -52,7 +75,7 @@ export default {
 			firstName: "",
 			lastName: "",
 			about: "",
-			adressLine: "",
+			adress: "",
 			country: "",
 			city: "",
 			state: "",
@@ -60,7 +83,10 @@ export default {
 			alertType: "",
 			greska: "false",
 			sent: "false",
-			email: "",  
+			email: "",
+            avgR: "",
+            imageRef: null,
+            imageURL: "",
 		};
 	},
     mounted() {
@@ -76,13 +102,18 @@ export default {
             this.firstName = `${doc.data().firstName}`;
             this.lastName = `${doc.data().lastName}`;
             this.city = `${doc.data().city}`;
+            this.avgR = `${doc.data().avgR}`;
             this.state = `${doc.data().state}`;
             this.email = `${doc.data().email}`;
             this.about = `${doc.data().about}`;
+            this.country = `${doc.data().country}`;
+            this.adress = `${doc.data().adress}`;
+            this.imageURL = `${doc.data().imageURL}`;
             
         }
       });
 		},
+        
          async resetPassword() {
       try {
         await sendPasswordResetEmail(auth, this.email)
@@ -93,16 +124,55 @@ export default {
             console.log(error);
          }
     },
-    save(){
-
+    async save(){
+                    const imageName = "userprofilepics/" + Date.now() + "_" + store.courentuser  + ".png";
+                    const storage = getStorage();
+                    const storageRef = ref(storage, imageName);
+                    if(this.imageRef != null){
+                    const dokument = await doc(db, "users", store.courentuser);
+                    uploadBytes(storageRef, this.imageRef.file).then((snapshot) => {
+                      
+                      getDownloadURL(ref(storage, storageRef, imageName))
+                    .then((url) => {
+                      updateDoc(dokument, {
+                        
+                      imageURL: url,
+                    });
+                      
+                    })
+                    
+                      
+                    });
+                    window.location.reload();
+                    }
+                    else{
+            const dokument = await doc(db, "users", store.courentuser);
+            await updateDoc(dokument, {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            city: this.city,
+            state: this.state,
+            country: this.country,
+            about: this.about,
+            adress: this.adress,
+            });
+            }
+            window.location.reload();
+		},
     }
  }
-}
+
 </script>
  
 
 
 <style>
+.slika {
+
+    margin-top: 25px;
+    
+}
+
 .card {
     border: none
 }
