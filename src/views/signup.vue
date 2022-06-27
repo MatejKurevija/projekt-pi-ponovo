2bfa910272775da88688167b254f71751cdd600e
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-27 19:53:01 +0200

    Update signup.vue

--- src/views/signup.vue
+++ src/views/signup.vue
@@ -9,50 +9,55 @@
               <div class = "form-group">
                 <label for = "exampleInputfirstname">First Name</label>
                 <input type = "firstname" class = "form-control" v-model= "firstname" id = "exampleInputfirstname"  
-                placeholder = "First Name" />
+                placeholder = "First Name" required />
               </div>
               <div class = "form group">
                 <label for = "exampleInputlastname">Last Name</label>
                 <input type = "lastname" class = "form-control" v-model="lastname" id = "exampleInputlastname"
-                placeholder = "Last Name"/>
+                placeholder = "Last Name" required/>
               </div>
                 <div class = "form group">
                 <label for = "exampleInputemail">Email</label>
-                <input type = "Email" class = "form-control" v-model="email" id = "exampleInputEmail" aria-describedby="emailHelp" placeholder = "Enter email" />
+                <input type = "Email" class = "form-control" v-model="email" id = "exampleInputEmail" aria-describedby="emailHelp" placeholder = "Enter email" required />
               </div>
                 <div class = "form group">
                 <label for = "exampleInputPassword">Password</label>
                 <input type = "password" class = "form-control" v-model="password" id = "exampleInputPassword"
-                placeholder = "Password"/>
+                placeholder = "Password" required />
               </div>
                 <div class = "form group">
                 <label for = "exampleInputCPassword">Confirm password</label>
                 <input type = "password"  class = "form-control" v-model="cpassword" id = "exampleInputCPassword"
-                placeholder = "Confirm your password"/>
+                placeholder = "Confirm your password" required />
               </div>
                 <div class = "form group">
                 <label for = "exampleInputCountry">Country</label>
                 <input type = "Country" class = "form-control" v-model="country" id = "exampleInputCountry"
-                placeholder = "Country"/>
+                placeholder = "Country" required />
                   <div class = "form group">
                 <label for = "exampleInputCity">City</label>
                 <input type = "City" class = "form-control" v-model="city" id = "exampleInputCity"
-                placeholder = "City"/>
+                placeholder = "City" required />
+              </div>
+              <div class = "form group">
+                <label for = "exampleInputCity">Adress</label>
+                <input type = "text" class = "form-control" v-model="adress" id = "exampleInputadress"
+                placeholder = "Adress" required />
               </div>
                 <div class = "form group">
                 <label for = "exampleInputState/prov">State/Provinance</label>
                 <input type = "State/prov" class = "form-control" v-model="state" id = "exampleInputState/prov"
-                placeholder = "State/Provinance"/>
+                placeholder = "State/Provinance" required />
               </div>
                 <div class = "form group">
                 <label for = "exampleInputDate">Date of birth</label>
                 <input type = "Date" class = "form-control" min="1900-01-01" max="2010-01-01" v-model="dateofb" id = "exampleInputDate"
-                placeholder = "Date Of birth"/>
+                placeholder = "Date Of birth" required />
               </div>
                 <div class = "form group">
                 <label for = "exampleInputabout_">About</label>
                 <input type = "about_" class = "form-control" v-model="about" id = "exampleInputabout_"
-                placeholder = "About you"/>
+                placeholder = "About you" required />
               </div>
               </div>
               <button type= "button" @click="signup" class = " btn btn-primary">Sign up</button>
@@ -70,7 +75,7 @@ import {
    auth,
    createUserWithEmailAndPassword,
    collection,
-   addDoc,
+   setDoc,
    doc,
    db,
 } from '@/firebase';
@@ -93,6 +98,7 @@ import {
           uid: "",
           about: "",
           avgR: "",
+          adress: "",
         };
     },
 
@@ -118,15 +124,20 @@ import {
                uid: res.user.uid,
                about: this.about,
                avgR: this.avgR,
-               email: this.email
+               email: this.email,
+               adress: this.adress,
             };
-       await addDoc(collection(db, "users"), userProfileData);
+       await setDoc(
+               doc(db, 'users', res.user.uid),
+               userProfileData
+            );
      
 
 
          } catch (error) {
             console.log(error);
          }
+         alert("Signed up succesfull");
          this.$router.replace({name: "home"});
       }
     },
