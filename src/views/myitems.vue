6ebba8bc31823e19dd1d868e0df2397b9f7e86ea
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-06-27 19:52:06 +0200

    Update myitems.vue

--- src/views/myitems.vue
+++ src/views/myitems.vue
@@ -1,63 +1,111 @@
-<template>
-     <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<template v-if="loaded">
+<div class="razmak">
+<h1> Here you can see the items you put to the store!!</h1>
+</div>
 
-<table class="table">
-  <thead class="thead-light">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+  <div class="app" >    
+    <myitems 
+    v-for="podatak in podatci "
+    :key="podatak.doc.id"
+    :title="podatak.title"
+    :createdat="podatak.createdat"
+    :adress="podatak.adress"
+    :country="podatak.country"
+    :desc="podatak.desc"
+    :region="podatak.region"
+    :renttime="podatak.renttime"
+    :value="podatak.value"
+    :zip="podatak.zip"
+    :city="podatak.city"
+    >
+    </myitems>
+</div>
+
+
 </template>
+<script>
+import store from "@/store"
+import {getDocs, collection , db, doc, getDoc} from "@/firebase"
+import myitems from '@/components/myitems'
+import { documentId } from '@firebase/firestore';
+
+
+
+export default {
+components: { myitems },
+name: myitems,
+        data() {
+            return {
+            loaded: false,
+            store,
+            podatci: [],
+
+            
+		};
+        },
+
+    async mounted() {
+        
+            await this.dohvatproizvoda();
+            this.loaded = true;
+            
+    },
+    
+
+ methods: {
+        async dohvatproizvoda(){
+                            const querySnapshot = await getDocs(collection(db, "products"));
+               
+
+                            let      trenutnipodatci = []
+
+                            querySnapshot.forEach((doc) => {   
+                             
+                                if(store.courentuser  == `${doc.data().renter}`) {
+                            trenutnipodatci = {"createdat": `${doc.data(String).Createdat}`, 
+                                                "title": `${doc.data(String).title}`,
+                                                "adress": `${doc.data(String).adress}`,
+                                                "isrented": `${doc.data(String).isrented}`,
+                                                "country": `${doc.data(String).country}`,
+                                                "desc": `${doc.data(String).desc}`,
+                                                "region": `${doc.data(String).region}`,
+                                                "value": `${doc.data(String).value}`,
+                                                "renter": `${doc.data(String).renter}`,
+                                                "zip": `${doc.data(String).zip}`,
+                                                "city": `${doc.data(String).city}`,
+                                                "doc": doc.id,
+                                                };
+                                
+                                this.podatci.push(trenutnipodatci);
+                                
+                                trenutnipodatci = []; 
+                                
+                                }
+                                },
+                                
+                            ); 
+
+                    },
+            
+            
+            
+    }
+
+};
+
+
+</script>
+
+<style scoped>
+.razmak {
+    
+    
+    text-align: center;
+    font-size: 15px;
+    color: black;
+    border-radius: 25px;
+    margin-top: 25px;
+    margin-bottom: 35px;
+    
+}
+</style>
